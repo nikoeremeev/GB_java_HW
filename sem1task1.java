ce19183892276a423befb25bc8d0942e3afea94a
Nikolay Eremeev <nikoeremeev@rambler.ru>
2022-06-20 19:15:50 +0300

    add task1

--- sem1task1.java
+++ sem1task1.java
@@ -4,7 +4,7 @@
 
 import java.util.Scanner;
 
-public class Exercise_01 {
+public class sem1task2 {
     public static void main(String[] args) {
         Scanner Scanner = new Scanner(System.in);
         System.out.print("Введите число : ");
